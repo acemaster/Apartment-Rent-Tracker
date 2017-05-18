49a20cc6900f6322748999ac339f172bdfee444d
acemaster <vivekhtc25@gmail.com>
2017-05-18 17:08:05 +0300

    Added initial files

---
+++ main/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Customer(models.Model):
+    """
+    Description: Customer 
+    ======================
+    check duration
+    """
+    user = models.OneToOneField(User)
+    mobile_no = models.CharField(max_length=100)
+    rent_paid = models.BooleanField(default=True)
+    def __unicode__(self):
+    	return self.user.first_name
+
